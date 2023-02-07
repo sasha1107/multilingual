bb9e0e8ca7459393d5a16c5375d758aff305a16b
정수현 <78977003+sasha1107@users.noreply.github.com>
2023-02-07 11:24:40 +0900

    Fix: 들여쓰기 수정

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ class TodoList {
 	 * 할 일을 추가할 수 있다.
 	 * 내용없이 추가할 수 없다.
 	 * @param {string} content - 내용
-   * @param {string} [category] - 카테고리
-   * @param {Tag[]} [tags] - 태그들
+         * @param {string} [category] - 카테고리
+         * @param {Tag[]} [tags] - 태그들
 	 */
 	createTodo(content, category, [tags]) {
 		// ...
@@ -49,9 +49,9 @@ class TodoList {
 	 * ID를 제외한 모든 속성을 수정할 수 있다.
 	 * 특정 할 일의 특정 태그를 수정할 수 있다.
 	 * @param {number} id - 아이디
-   * @param {string} [content] - 내용
-   * @param {boolean} [isDone] - 완료여부
-   * @param {string} [category] - 카테고리
+         * @param {string} [content] - 내용
+         * @param {boolean} [isDone] - 완료여부
+         * @param {string} [category] - 카테고리
 	 * @param {Tag[]} [tags] - 태그들
 	 */
 	updateTodo(id, tagId) {
@@ -60,7 +60,7 @@ class TodoList {
 
 	/**
 	 * ID를 기반으로 특정 할 일을 삭제할 수 있다.
-   * id를 넘기지 않으면 모든 할 일을 제거할 수 있다. 
+         * id를 넘기지 않으면 모든 할 일을 제거할 수 있다. 
 	 * @param {number} [id] - 아이디
 	 */
 	deleteTodo(id) {
@@ -69,9 +69,9 @@ class TodoList {
 
 	/**
 	 * 특정 할 일의 특정 태그나 모든 태그를 제거할 수 있다.
-   * 두 번째 인자로 태그를 넘기지 않으면 모든 태그를 제거한다.
+         * 두 번째 인자로 태그를 넘기지 않으면 모든 태그를 제거한다.
 	 * @param {number} id - 아이디
-   * @param {Tag[]} [tags] - 태그들
+         * @param {Tag[]} [tags] - 태그들
 	 */
 	deleteTags(id, tags) {
 		// ...
