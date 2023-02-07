ce17b2349a731cc1fc55c0db6a7cea27a2166f55
Soohyun Jung <sasha1107@naver.com>
2023-02-07 10:20:50 +0900

    Feat: JSDoc 환경설정

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ * @param {number} age
+ * @returns {string} name + age
+ */
+function hello(name, age) {
+  return `name : ${name} , age : ${age}`;
+}
+  
+hello('picpal' , 10);
